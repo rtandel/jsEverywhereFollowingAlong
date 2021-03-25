51c11344e20933c61b9d9c8a12988bf3903480f8
Rajen Tandel <rajentandel@Rajens-MacBook-Pro.local>
2021-03-25 12:17:01 -0400

    This is the last try for now

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,51 +1,68 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
+import ReactMarkdown from 'react-markdown';
+import { useQuery, gql } from '@apollo/client';
 
 import NoteFeed from '../components/NoteFeed';
+
+const GET_NOTES = gql`
+    query noteFeed($cursor:String){
+  noteFeed(cursor:$cursor) {
+    cursor
+    hasNextPage
+    notes {
+      id
+      createdAt
+      content
+      favoriteCount
+      author {
+        username
+        id
+        avatar
+      }
+    }
+  }
+}
+`;
+
+
 import Button from '../components/Button';
-import { GET_NOTES } from '../gql/query';
 
 const Home = () => {
-  // query hook
-  const { data, loading, error, fetchMore } = useQuery(GET_NOTES);
-  // if the data is loading, display a loading message
-  if (loading) return <p>Loading...</p>;
-  // if there is an error fetching the data, display an error message
-  if (error) return <p>Error!</p>;
+    const { data, loading, error, fetchMore } = useQuery(GET_NOTES);
 
-  // if the data is successful, display the data in our UI
-  return (
-    <React.Fragment>
-      <NoteFeed notes={data.noteFeed.notes} />
-      {data.noteFeed.hasNextPage && (
-        <Button
-          onClick={() =>
-            fetchMore({
-              variables: {
-                cursor: data.noteFeed.cursor
-              },
-              updateQuery: (previousResult, { fetchMoreResult }) => {
-                return {
-                  noteFeed: {
-                    cursor: fetchMoreResult.noteFeed.cursor,
-                    hasNextPage: fetchMoreResult.noteFeed.hasNextPage,
-                    // combine the new results and the old
-                    notes: [
-                      ...previousResult.noteFeed.notes,
-                      ...fetchMoreResult.noteFeed.notes
-                    ],
-                    __typename: 'noteFeed'
-                  }
-                };
-              }
-            })
-          }
-        >
-          Load more
-        </Button>
-      )}
-    </React.Fragment>
-  );
-};
+    if (loading) return <p>Loading</p>;
+
+    if (error) return <p>Error</p>;
+
+    return (
+        <React.Fragment>
+            <NoteFeed notes={data.noteFeed.notes} />
+            {data.noteFeed.hasNextPage && (
+                <Button
+                    onClick={() => 
+                        fetchMore({
+                            variables: {
+                                cursor: data.noteFeed.cursor
+                            },
+                            updateQuery: (previousResult, { fetchMoreResult }) => {
+                                return {
+                                    noteFeed: {
+                                        cursor: fetchMoreResult.noteFeed.cursor,
+                                        hasNextPage: fetchMoreResult.noteFeed.hasNextPage,
+                                        notes: [
+                                            ...previousResult.noteFeed.notes,
+                                            ...fetchMoreResult.noteFeed.notes
+                                        ],
+                                        __typename: 'noteFeed'
+                                    }
+                                }
+                            }   
+                        }
+                        )}
+                >Load More</Button>
+            )}
+        </React.Fragment>
+    )
+}
 
 export default Home;
