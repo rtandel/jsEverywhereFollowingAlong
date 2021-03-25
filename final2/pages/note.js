930695b9ba618c92ae167785721a57dcae2c77ce
Rajen Tandel <rajentandel@Rajens-MacBook-Pro.local>
2021-03-25 10:46:21 -0400

    trying something

---
+++ final2/pages/note.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+import Note from '../components/Note';
+import { GET_NOTE }  from '../gql/query';
+
+const NotePage = props => {
+    const id = props.match.params.id;
+
+    const { loading, error, data } = useQuery(GET_NOTE, { variables: { id }});
+
+    if (loading) {
+        return <p>Loading</p>
+    } 
+    if (error) {
+        return <p>Error</p>
+    }
+    return (
+        <Note note={data.note} />
+    )
+}
+
+export default NotePage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+// import React and our routing dependencies
+import React from 'react';
 import { useQuery, gql } from '@apollo/client';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+
+// import our shared layout component
 import Layout from '../components/Layout';
 
-
+// import our routes
 import Home from './home';
 import MyNotes from './mynotes';
 import Favorites from './favorites';
-import NotePage from './note';
+import Note from './note';
 import SignUp from './signup';
 import SignIn from './signin';
 import NewNote from './new';
 import EditNote from './edit';
-
 
 const IS_LOGGED_IN = gql`
   {
@@ -20,47 +22,47 @@ const IS_LOGGED_IN = gql`
   }
 `;
 
+// define our routes
 const Pages = () => {
-    return (
-        <Router>
-            <Layout>
-                <Route exact path="/" component={Home} />
-                <PrivateRoute path="/mynotes" component={MyNotes} />
-                <PrivateRoute path="/favorites" component={Favorites} />
-                <PrivateRoute path="/new" component={NewNote} />
-                <PrivateRoute path="/edit/:id" component={EditNote} />
-                <Route path="/note/:id" component={NotePage} />
-                <Route path="/signup" component={SignUp} />
-                <Route path="/signin" component={SignIn} />
-            </Layout>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Layout>
+        <Route exact path="/" component={Home} />
+        <PrivateRoute path="/mynotes" component={MyNotes} />
+        <PrivateRoute path="/favorites" component={Favorites} />
+        <Route path="/note/:id" component={Note} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/signin" component={SignIn} />
+        <PrivateRoute path="/new" component={NewNote} />
+        <PrivateRoute path="/edit/:id" component={EditNote} />
+      </Layout>
+    </Router>
+  );
+};
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-    const { loading, error, data } = useQuery(IS_LOGGED_IN);
-
-    if (loading) return <p>Loading...</p>
-
-    if (error) return <p>Error!</p>
-
-    return (
-        <Route
-            {...rest}
-            render={props =>
-                data.isLoggedIn === true ? (
-                    <Component {...props} />
-                ) : (
-                    <Redirect
-                        to={{
-                            pathname: '/signin',
-                            state: { from: props.location }
-                        }}
-                    />
-                )
-            }
-            />
-    )
-}
+  const { loading, error, data } = useQuery(IS_LOGGED_IN);
+  // if the data is loading, display a loading message
+  if (loading) return <p>Loading...</p>;
+  // if there is an error fetching the data, display an error message
+  if (error) return <p>Error!</p>;
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        data.isLoggedIn === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/signin',
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+};
 
 export default Pages;
